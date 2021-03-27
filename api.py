228181431bdcda87f2dac1a3d18714507e4a9a93
Satyajit Kamble <satya@10-248-118-28.wireless.oregonstate.edu>
2021-03-26 22:39:25 -0700

    Added socketio events and listners on server and dummy client

--- api.py
+++ api.py
@@ -12,22 +12,58 @@ login = LoginManager(app)
 dbmongo = MongoEngine(app)
 bootstrap = Bootstrap(app)
 
-from flask_socketio import SocketIO
+from flask_socketio import SocketIO, join_room, leave_room, send, emit
 socketio = SocketIO(app)
-
+from collections import deque
+import uuid
+import json
 from app import routes
 from blueprints.authentication import authenticationController
 import models
 
 app.register_blueprint(authenticationController)
 
+
+queue = deque([])
+
+
+    
+
+
 @app.route("/chat")
 def render_chat():
     return render_template('social.html')
 
-@socketio.on('my event')
-def handle_my_custom_event(json, methods=['GET', 'POST']):
-    print('received my event: ' + str(json))
+
+@socketio.on('message')
+def message(data, methods=['GET', 'POST']):
+    room_id = data['room']
+    emit('message', data['message'], room=room_id)
+
+
+
+@socketio.on('pair me')
+def pairIfPossible(data, methods=['GET', 'POST']):
+    #data = json.loads(data)
+    if queue:
+        #we found a match
+        waiting_user, room_id = queue.popleft()
+        join_room(room_id)
+        paired = {'p1':waiting_user, 'p2':data['username']}
+        emit('introduction', paired, room=room_id, json = True)
+    else:
+        #keep user in waiting queue
+        room_id = str(uuid.uuid4())
+        join_room(room_id)
+        queue.append((data['username'],room_id))
+    
+    print(queue)
+
+# @socketio.on('disconnect')
+# def test_disconnect():
+    
+
+
 
 if __name__ == "__main__":
     socketio.run(app, debug=True)
