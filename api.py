02b4d6a07b09b45a4884287592320c8c3edd7146
Raj <rajnandu2711@gmail.com>
2021-03-26 19:14:44 -0700

    flask-socket testing

--- api.py
+++ api.py
@@ -1,9 +1,33 @@
-from flask import Flask
+from flask import Flask, render_template
 from blueprints.authentication import authenticationController
-app = Flask(__name__)
-app.register_blueprint(authenticationController)
+from flask_socketio import SocketIO
+from flask_cors import CORS
 
-from app import routes
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+app.register_blueprint(authenticationController)
+# socketio = SocketIO(app)
+socketio = SocketIO(app, async_mode='eventlet')
+# cors = CORS(app, resources={r"/*": {"origins": "*"}})
+
+@socketio.on('connect')
+def test_connect():
+    emit('my response', {'data': 'Connected'})
+
+@socketio.on('disconnect')
+def test_disconnect():
+    print('Client disconnected')
+
+@socketio.on('my event')
+def handle_my_custom_event(json):
+    emit('my response', {'data': 42})
+
+@app.route("/chat", methods=["GET"])
+def render_chat():
+    return render_template('social.html')
 
 if __name__ == "__main__":
-    app.run(host = "0.0.0.0",port = 5000, debug=True)
+    socketio.run(app)
+    app.run(debug=True, host='0.0.0.0')
+    
