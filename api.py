2e58e4a231390adde11ae324933ee795fee03bb6
Miklos Bowling <miklos.bowling@gmail.com>
2021-03-26 20:36:50 -0700

    Basic socket functionality

--- api.py
+++ api.py
@@ -1,33 +1,23 @@
 from flask import Flask, render_template
-from blueprints.authentication import authenticationController
+# from blueprints.authentication import authenticationController
 from flask_socketio import SocketIO
-from flask_cors import CORS
+# from flask_cors import CORS
 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
-app.register_blueprint(authenticationController)
+# app.register_blueprint(authenticationController)
 # socketio = SocketIO(app)
-socketio = SocketIO(app, async_mode='eventlet')
+socketio = SocketIO(app)
 # cors = CORS(app, resources={r"/*": {"origins": "*"}})
 
-@socketio.on('connect')
-def test_connect():
-    emit('my response', {'data': 'Connected'})
-
-@socketio.on('disconnect')
-def test_disconnect():
-    print('Client disconnected')
-
-@socketio.on('my event')
-def handle_my_custom_event(json):
-    emit('my response', {'data': 42})
-
-@app.route("/chat", methods=["GET"])
+@app.route("/chat")
 def render_chat():
     return render_template('social.html')
 
+@socketio.on('my event')
+def handle_my_custom_event(json, methods=['GET', 'POST']):
+    print('received my event: ' + str(json))
+
 if __name__ == "__main__":
-    socketio.run(app)
-    app.run(debug=True, host='0.0.0.0')
-    
+    socketio.run(app, debug=True)
